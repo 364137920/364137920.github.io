ba1898b9fb31925572ee88f941b689ab34a0bd55
364137920 <364137920@qq.com>
2020-12-18 13:39:39 +0800

    修改

--- myworks/data.js
+++ myworks/data.js
@@ -1,1 +1,12 @@
-var myData={designer:{name:'李俊达',phone:'微信 364137920',tag:["字体设计师"],bg:'#0042aa'},recommend:{contact:'',city:''},gallery:[{src:"myworks/十面埋伏.png",w:1460,h:2530},{src:"myworks/再见二丁目.png",w:1460,h:2530},{src:"myworks/性空山.png",w:1461,h:2531},{src:"myworks/终生美丽.png",w:1461,h:2531},{src:"myworks/陈奕迅《浮夸》.png",w:1460,h:2530},]}
+var myData = {
+    designer: {
+        name: '李俊达',
+        phone: '364137920',
+        tag: ["#字体设计师 前程序员"], bg: '#0042aa'},
+        recommend: {contact: '微信 364137920', city: ''},
+    gallery: [{src: "myworks/十面埋伏.png", w: 1460, h: 2530},
+        {src: "myworks/再见二丁目.png", w: 1460, h: 2530},
+        {src: "myworks/性空山.png", w: 1461, h: 2531},
+        {src: "myworks/终生美丽.png", w: 1461, h: 2531},
+        {src: "myworks/陈奕迅《浮夸》.png", w: 1460, h: 2530},]
+}
