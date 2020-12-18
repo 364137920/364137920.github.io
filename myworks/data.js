c283e5882161066db44ff831973a3618c59ae018
364137920 <364137920@qq.com>
2020-12-18 21:24:07 +0800

    K

--- myworks/data.js
+++ myworks/data.js
@@ -3,7 +3,7 @@ var myData = {
         name: '李俊达',
         //phone: '364137920',
         tag: ["微信 simple_2046"], bg: '#0042aa'},
-        recommend: {contact: '微信 364137920', city: ''},
+        //recommend: {contact: '微信 364137920', city: ''},
     gallery: [{src: "myworks/十面埋伏.png", w: 1460, h: 2530},
         {src: "myworks/再见二丁目.png", w: 1460, h: 2530},
         {src: "myworks/性空山.png", w: 1461, h: 2531},
