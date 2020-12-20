293c7b79bf77815d3ce75ee9c3a283cf71f9c013
364137920 <364137920@qq.com>
2020-12-21 00:37:38 +0800

    Merge remote-tracking branch 'origin/master'

--- myworks/data.js
+++ myworks/data.js
@@ -2,31 +2,11 @@ var myData = {
     designer: {name: '李俊达', phone: '', tag: ["微信simple_2046"], bg: '#1e2025'},
     recommend: {contact: '', city: ''},
     gallery: [{
-        src: "myworks/01性空山.png",
-        w: 1461,
-        h: 2531
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/01性空山.png",
-        w: 1461,
-        h: 2531
-    }, {
-        src: "myworks/02再见二丁目.png",
-        w: 1460,
-        h: 2530
-    }, {
         src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/02再见二丁目.png",
         w: 1460,
         h: 2530
     }, {
-        src: "myworks/03十面埋伏.png",
-        w: 1460,
-        h: 2530
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/03十面埋伏.png",
-        w: 1460,
-        h: 2530
-    }, {
-        src: "myworks/05终生美丽.png",
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/01性空山.png",
         w: 1461,
         h: 2531
     }, {
@@ -34,56 +14,32 @@ var myData = {
         w: 1461,
         h: 2531
     }, {
-        src: "myworks/11天机十二宫.png",
-        w: 512,
-        h: 882
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/03陈奕迅《浮夸》.png",
+        w: 1460,
+        h: 2530
     }, {
         src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/11天机十二宫.png",
         w: 512,
         h: 882
     }, {
-        src: "myworks/12抖音湖人.png",
-        w: 698,
-        h: 1106
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/03十面埋伏.png",
+        w: 1460,
+        h: 2530
     }, {
         src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/12抖音湖人.png",
         w: 698,
         h: 1106
     }, {
-        src: "myworks/08我和我的家乡.png",
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/10金刚川.png",
+        w: 1461,
+        h: 2531
+    }, {
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/09八佰.png",
         w: 1461,
         h: 2531
     }, {
         src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/08我和我的家乡.png",
         w: 1461,
         h: 2531
-    }, {
-        src: "myworks/03陈奕迅《浮夸》.png",
-        w: 1460,
-        h: 2530
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/03陈奕迅《浮夸》.png",
-        w: 1460,
-        h: 2530
-    }, {
-        src: "myworks/07金刚狼.png",
-        w: 1242,
-        h: 2208
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/07金刚狼.png",
-        w: 1242,
-        h: 2208
-    }, {
-        src: "myworks/10金刚川.png",
-        w: 1461,
-        h: 2531
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/10金刚川.png",
-        w: 1461,
-        h: 2531
-    }, {
-        src: "myworks/09八佰.png",
-        w: 1461,
-        h: 2531
-    }, {src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/09八佰.png", w: 1461, h: 2531},]
+    }, {src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/07金刚狼.png", w: 1242, h: 2208},]
 }
