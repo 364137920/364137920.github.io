3c66c18529039dadc38bad93e18196e6000018d1
364137920 <364137920@qq.com>
2020-12-20 22:38:28 +0800

    Merge remote-tracking branch 'origin/master'

--- myworks/data.js
+++ myworks/data.js
@@ -2,64 +2,44 @@ var myData = {
     designer: {name: '李俊达', phone: '', tag: ["微信simple_2046"], bg: '#1e2025'},
     recommend: {contact: '', city: ''},
     gallery: [{
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/02性空山.png",
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/01性空山.png",
         w: 1461,
         h: 2531
     }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/06十面埋伏.png",
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/02再见二丁目.png",
         w: 1460,
         h: 2530
     }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/抖音湖人.png",
-        w: 698,
-        h: 1106
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/04浮夸.png",
+        w: 288,
+        h: 499
     }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/01 陈奕迅《浮夸》.png",
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/03十面埋伏.png",
         w: 1460,
         h: 2530
     }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/04金刚川1.png",
-        w: 1125,
-        h: 1714
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/10金刚川.png",
+        w: 192,
+        h: 332
     }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/画板 4.png",
-        w: 1080,
-        h: 1920
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/08我和我的家乡.png",
+        w: 192,
+        h: 332
     }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/03再见二丁目.png",
-        w: 1460,
-        h: 2530
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/05 画板 4 2.png",
-        w: 1080,
-        h: 1920
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/07金刚狼.png",
-        w: 1242,
-        h: 2208
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/终生美丽.png",
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/05终生美丽.png",
         w: 1461,
         h: 2531
     }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/我和我的家乡.png",
-        w: 1080,
-        h: 1008
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/天机十二宫.PNG",
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/11天机十二宫.png",
         w: 512,
         h: 882
     }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/图层 5.PNG",
-        w: 1242,
-        h: 2208
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/09八佰.png",
+        w: 288,
+        h: 499
     }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/IMG_8729.JPG",
-        w: 1080,
-        h: 1270
-    }, {
-        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/咸鱼仔.png",
-        w: 1080,
-        h: 1920
-    }, {src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/IMG_8731.JPG", w: 1440, h: 1080},]
+        src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/12抖音湖人.png",
+        w: 698,
+        h: 1106
+    }, {src: "https://cdn.jsdelivr.net/gh/364137920/364137920.github.io/myworks/07金刚狼.png", w: 1242, h: 2208},]
 }
